673b0027c20bf1392319dc8eda493b420820679e
alexcameron89 <alexcameron98@gmail.com>
2014-08-22 16:03:40 -0500

    Adds ratio to Profile#Show page, and adds ratio method to Bean Model

--- app/models/bean.rb
+++ app/models/bean.rb
@@ -1,4 +1,9 @@
 class Bean < ActiveRecord::Base
 	has_many :ingredients
 	has_many :profiles, :through => :ingredients
+
+  def ratio
+    i = Ingredient.find_by bean_id: self.id
+    i.quantity
+  end
 end
