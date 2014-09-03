d56ae1a137b6a65943bdc52da0d222151eb1668c
alexcameron89 <alexcameron98@gmail.com>
2014-09-02 19:06:36 -0500

    Style Change: Changed beanlist method to bean_list

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,12 +1,13 @@
 class Profile < ActiveRecord::Base
 	has_many :ingredients
+	has_many :orders, :dependent => :destroy
 	has_many :beans, :through => :ingredients
 	before_save :check_ratio
 
 	def check_ratio
 	end
 
-	def beanlist
+	def bean_list
 		bean_array = []
 		Bean.all.each do |b|
 			bean_array << [b.name, b.id]
